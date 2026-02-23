e1232905dbeca53c19bd1ee17626f76b439bb421
hussein34535 <hussein34535@proton.me>
2026-02-23 21:11:06 +0200

    g

--- hawsni_web/src/lib/axios.ts
+++ hawsni_web/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:5002/api',
+    baseURL: 'https://hwasibackend.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
