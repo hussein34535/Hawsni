e7f5c8a8d84c07ec476122b7140efbbde1ff92eb
hussein34535 <hussein34535@proton.me>
2026-02-25 06:08:27 +0200

    h

--- hawsni_web/next.config.ts
+++ hawsni_web/next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bdgwkcenzmeuvwmcjhfi.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'hawsni.com',
+      }
+    ],
+  },
 };
-
 export default nextConfig;
