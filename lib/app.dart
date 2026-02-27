a6d25b6605c0c9ad655b6c6a0c3c4f83a0a16eab
hussein34535 <hussein34535@proton.me>
2026-02-27 02:59:07 +0200

    ب

--- lib/app.dart
+++ lib/app.dart
@@ -7,6 +7,28 @@ import 'package:hwasi_app/core/themes/app_theme.dart';
 import 'package:hwasi_app/l10n/generated/app_localizations.dart';
 import 'package:hwasi_app/core/router/app_router.dart';
 
+/// Forces LTR direction even for Arabic locale
+class _LTRWidgetsLocalizations extends DefaultWidgetsLocalizations {
+  @override
+  TextDirection get textDirection => TextDirection.ltr;
+}
+
+class _LTRWidgetsDelegate extends LocalizationsDelegate<WidgetsLocalizations> {
+  const _LTRWidgetsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => true;
+
+  @override
+  Future<WidgetsLocalizations> load(Locale locale) async =>
+      _LTRWidgetsLocalizations();
+
+  @override
+  bool shouldReload(
+          covariant LocalizationsDelegate<WidgetsLocalizations> old) =>
+      false;
+}
+
 class App extends StatefulWidget {
   const App({super.key});
 
@@ -19,31 +41,22 @@ class _AppState extends State<App> {
   Widget build(BuildContext context) {
     return Consumer<SettingsProvider>(
       builder: (context, settingsProvider, child) {
-        return Directionality(
-          textDirection: TextDirection.ltr,
-          child: MaterialApp.router(
-            title: 'HWASI',
-            locale: Locale(settingsProvider.language),
-            localizationsDelegates: [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-              GlobalWidgetsLocalizations.delegate,
-              GlobalCupertinoLocalizations.delegate,
-            ],
-            supportedLocales: AppLocalizations.supportedLocales,
-            themeMode:
-                settingsProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            scrollBehavior: WebScrollBehavior(),
-            routerConfig: AppRouter.router,
-            builder: (context, child) {
-              return Directionality(
-                textDirection: TextDirection.ltr,
-                child: child!,
-              );
-            },
-          ),
+        return MaterialApp.router(
+          title: 'HWASI',
+          locale: Locale(settingsProvider.language),
+          localizationsDelegates: [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            const _LTRWidgetsDelegate(), // Force LTR layout
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          themeMode:
+              settingsProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          scrollBehavior: WebScrollBehavior(),
+          routerConfig: AppRouter.router,
         );
       },
     );
